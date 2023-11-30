5c06a2166b8fea9a2f7f0bf048d2b179fa06ea00
DVictorB <david.balboa@tecsup.edu.pe>
2023-11-30 17:01:05 -0500

    delete this changes

---
+++ app/src/main/java/com/victor/balboa/poketinder_2023/ui/adapter/MyPokemonsAdapter.kt
@@ -0,0 +1,25 @@
+package com.victor.balboa.poketinder_2023.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.victor.balboa.poketinder_2023.data.database.entities.MyPokemonEntity
+import com.victor.balboa.poketinder_2023.databinding.ItemPokemonSavedBinding
+import com.victor.balboa.poketinder_2023.ui.holder.MyPokemonsHolder
+
+class MyPokemonsAdapter(val list: List<MyPokemonEntity>):
+    RecyclerView.Adapter<MyPokemonsHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPokemonsHolder {
+        val view = ItemPokemonSavedBinding.inflate(LayoutInflater.from((parent.context)), parent,false)
+        return MyPokemonsHolder(view.root)
+    }
+
+    override fun onBindViewHolder(holder: MyPokemonsHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+}
