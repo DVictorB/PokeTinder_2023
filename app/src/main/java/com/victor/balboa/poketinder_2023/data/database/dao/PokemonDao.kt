9bc233b2cf5424a46f3401bc405de57579da578a
DVictorB <david.balboa@tecsup.edu.pe>
2023-12-02 04:45:05 -0500

    feature/add changes swap

--- app/src/main/java/com/victor/balboa/poketinder_2023/data/database/dao/PokemonDao.kt
+++ app/src/main/java/com/victor/balboa/poketinder_2023/data/database/dao/PokemonDao.kt
@@ -4,16 +4,33 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.victor.balboa.poketinder_2023.data.database.entities.MyPokemonEntity
 
 @Dao
 interface PokemonDao {
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertIfNotExists(pokemon: MyPokemonEntity)
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(pokemon: MyPokemonEntity)
 
+    // Después de insertar, actualiza la lista
+    @Transaction
+    suspend fun insertAndUpdateList(pokemon: MyPokemonEntity) {
+        insert(pokemon)
+        // Recarga la lista después de la inserción
+        updateList()
+    }
+
+
     @Query("SELECT * FROM pokemon_table")
     suspend fun getAllPokemons() : List<MyPokemonEntity>
+
+    // Método para actualizar la lista
+    @Query("SELECT * FROM pokemon_table")
+    suspend fun updateList(): List<MyPokemonEntity>
 
     @Query("DELETE FROM pokemon_table")
     suspend fun deleteTable()
--- app/src/main/java/com/victor/balboa/poketinder_2023/ui/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/victor/balboa/poketinder_2023/ui/viewmodel/FavoriteViewModel.kt
@@ -10,6 +10,7 @@ import com.victor.balboa.poketinder_2023.data.database.entities.MyPokemonEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FavoriteViewModel(): ViewModel(){
 
@@ -17,18 +18,47 @@ class FavoriteViewModel(): ViewModel(){
 
     val myPokemonList = MutableLiveData<List<MyPokemonEntity>>()
 
-    fun getMyPokemons(context: Context){
-        CoroutineScope(Dispatchers.IO).launch {
-            val myPokemons = getRoomDataBase(context).getPokemonDao().getAllPokemons()
-            myPokemonList.postValue(myPokemons)
+    private var isDataLoaded = false
+
+    fun getMyPokemons(context: Context) {
+        if (!isDataLoaded) {
+            viewModelScope.launch {
+                val myPokemons = withContext(Dispatchers.IO) {
+                    getRoomDataBase(context).getPokemonDao().getAllPokemons()
+                }
+                // Filtra los duplicados antes de asignar a myPokemonList
+                val uniquePokemons = myPokemons.distinctBy { it.idPokemon }
+                myPokemonList.postValue(uniquePokemons)
+            }
+            isDataLoaded = true
         }
     }
 
-    fun deleteAllPokemon(context: Context){
-        viewModelScope.launch {
-            getRoomDataBase(context).getPokemonDao().deleteTable()
-            myPokemonList.postValue(emptyList())
+    // Método para cargar los Pokémon después de realizar operaciones de inserción o eliminación
+    suspend fun loadPokemons(context: Context) {
+        val myPokemons = withContext(Dispatchers.IO) {
+            getRoomDataBase(context).getPokemonDao().getAllPokemons()
         }
+        // Filtra los duplicados antes de asignar a myPokemonList
+        val uniquePokemons = myPokemons.distinctBy { it.idPokemon }
+
+        myPokemonList.postValue(uniquePokemons)
+    }
+
+    suspend fun addPokemon(context: Context, pokemon: MyPokemonEntity) {
+        withContext(Dispatchers.IO) {
+            getRoomDataBase(context).getPokemonDao().insertAndUpdateList(pokemon)
+        }
+        // Después de agregar, carga los Pokémon nuevamente
+        loadPokemons(context)
+    }
+
+    suspend fun deleteAllPokemon(context: Context) {
+        withContext(Dispatchers.IO) {
+            getRoomDataBase(context).getPokemonDao().deleteTable()
+        }
+        // Después de eliminar, carga los Pokémon nuevamente
+        loadPokemons(context)
     }
 
     private fun getRoomDataBase(context: Context): PokemonDatabase {
